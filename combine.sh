96b557f0c587d8accf801dd019519b257cb31814
AlekzNet <alekz.net@gmail.com>
2017-02-05 02:25:04 +0100

    Update combine.sh

--- combine.sh
+++ combine.sh
@@ -9,7 +9,7 @@ for acl in `ls */*acl | sed -e 's%^.*/%%' | sort -u`
 do
 	for dir in src dst
 	do
-#		eval cnt${dir}=`ipaclmatch.py --noany -t --direct --permit --$dir -a $IPS $acl | optimacl.py | wc -l | awk '{print $1}'`
+#		eval cnt${dir}=`cat */$acl | ipaclmatch.py --noany --direct -t --permit --$dir -a $IPS - | optimacl.py | wc -l | awk '{print $1}'``
 		eval cnt${dir}=`cat */$acl | ipaclmatch.py --noany -t --permit --$dir -a $IPS - | optimacl.py | wc -l | awk '{print $1}'`
 
 	done
