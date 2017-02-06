b363e7be6420190bbab2c73c08123491eff264b8
AlekzNet <alekz.net@gmail.com>
2017-02-06 02:50:23 +0100

    Restructured the file

--- ipaclmatch.py
+++ ipaclmatch.py
@@ -12,50 +12,6 @@ except ImportError:
 	print >>sys.stderr, 'ERROR: netaddr module not found.'
 	sys.exit(1)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-a','--addr', default='0.0.0.0/0', help="Comma-separated list of addresses/netmasks. \"all\" shows all lines")
-parser.add_argument('acl', default="-", nargs='?', help="Cisco ASA ACL filename or \"-\" to read from the console (default)")
-sd = parser.add_mutually_exclusive_group()
-sd.add_argument('-s', '--src', help="Search the source", action="store_true")
-sd.add_argument('-d', '--dst', help="Search the destination", action="store_true")
-sd.add_argument('-b', '--both', help="Search both the source and the destination (default)", action="store_true")
-an = parser.add_mutually_exclusive_group()
-an.add_argument('--noany', help="Ignore \'any\' in the ACLs", action="store_true")
-an.add_argument('--any', help="Show only \'any\' in the ACLs", action="store_true")
-dp = parser.add_mutually_exclusive_group()
-dp.add_argument('--deny', help="Search \'deny\' rules only" , action="store_true")
-dp.add_argument('--permit', help="Search \'permit\' rules only" , action="store_true")
-ra = parser.add_mutually_exclusive_group()
-ra.add_argument('--range', default=True, help="Replace lt, gt, and neq with ranges (default)", action="store_true")
-ra.add_argument('--norange', default=False, help="Replace lt, gt, and neq with \<, \>, and ! symbols", action="store_true")
-parser.add_argument('--direct', help="Direct IP match only" , action="store_true")
-parser.add_argument('-t','--transform', help='Transform the output. Must be used with either -s or -d and with either --deny or --permit', action="store_true")
-parser.add_argument('-p','--policy', help='Print the policy in the form:\n SourceIP SourceMask DestIP DestMask Proto:Port. Must be used with either --deny or --permit', action="store_true")
-parser.add_argument('--contain', help='Direct matches and subnets (not direct and uppernets). Assumes --noany', action="store_true")
-parser.add_argument('--noline', help='Removes line number from the output', action="store_true")
-args = parser.parse_args()
-
-if not args.src and not args.dst and not args.both: args.both = True
-if "all" in args.addr: args.addr="0.0.0.0/0"
-if "0.0.0.0/0" in args.addr and not args.any: args.contain=True
-if args.both and args.transform:
-	print >>sys.stderr, "--transform requires either --src or --dst. --transform cannot be used with --both"
-	sys.exit(1)
-if args.policy: args.transform=True
-if args.both and args.direct:
-	print >>sys.stderr, "--direct requires either --src or --dst. --both cannot be used with --direct"
-	sys.exit(1)
-
-if args.norange: args.range=False
-
-
-
-# service name - port mapping from
-# http://www.cisco.com/c/en/us/td/docs/security/asa/asa96/configuration/general/asa-96-general-config/ref-ports.html#ID-2120-000002b8
-if args.transform:
-	s2n={'domain': '53', 'sunrpc': '111', 'citrix-ica': '1494', 'telnet': '23', 'tftp': '69', 'syslog': '514', 'rtsp': '554', 'secureid-udp': '5510', 'gopher': '70', 'h323': '1720', 'echo': '7', 'netbios-ssn': '139', 'snmptrap': '162', 'rpc': '111', 'radius': '1645', 'pcanywhere-data': '5631', 'nameserver': '42', 'rsh': '514', 'sqlnet': '1521', 'uucp': '540', 'ftp': '21', 'sip': '5060', 'whois': '43', 'smtp': '25', 'ctiqbe': '2748', 'hostname': '101', 'snmp': '161', 'mobile-ip': '434', 'daytime': '13', 'ldaps': '636', 'isakmp': '500', 'netbios-dgm': '138', 'finger': '79', 'https': '443', 'ldap': '389', 'kshell': '544', 'irc': '194', 'nntp': '119', 'biff': '512', 'http': '80', 'cifs': '3020', 'exec': '512', 'pptp': '1723', 'ntp': '123', 'aol': '5190', 'talk': '517', 'pcanywhere-status': '5632', 'pop3': '110', 'pop2': '109', 'ftp-data': '20', 'lotusnotes': '1352', 'rip': '520', 'xdmcp': '177', 'pim-auto-rp': '496', 'login': '513', 'dnsix': '195', 'ident': '113', 'netbios-ns': '137', 'kerberos': '750', 'tacacs': '49', 'who': '513', 'cmd': '514', 'bootps': '67', 'bgp': '179', 'nfs': '2049', 'klogin': '543', 'chargen': '19', 'www': '80', 'time': '37', 'discard': '13', 'imap4': '143', 'lpd': '515', 'bootpc': '68', 'radius-acct': '1646', 'ssh': '22', 'redirect': '5', 'information-reply': '16', 'alternate-address': '6', 'mask-reply': '18', 'timestamp-request': '13', 'router-solicitation': '10', 'mobile-redirect': '32', 'parameter-problem': '12', 'echo': '8', 'timestamp-reply': '14', 'conversion-error': '31', 'information-request': '15', 'unreachable': '3', 'echo-reply': '0', 'source-quench': '4', 'mask-request': '17', 'time-exceeded': '11', 'router-advertisement': '9'}
-
-
 # True if the IP belongs to the Source IP
 # arr[7] -- source IP-address or host
 # arr[8] -- netmask or hostip
@@ -193,6 +149,48 @@ def serv2num(f):
 		print >>sys.stderr, arr[f] + " is not a known service"
 		sys.exit(1)
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-a','--addr', default='0.0.0.0/0', help="Comma-separated list of addresses/netmasks. \"all\" shows all lines")
+parser.add_argument('acl', default="-", nargs='?', help="Cisco ASA ACL filename or \"-\" to read from the console (default)")
+sd = parser.add_mutually_exclusive_group()
+sd.add_argument('-s', '--src', help="Search the source", action="store_true")
+sd.add_argument('-d', '--dst', help="Search the destination", action="store_true")
+sd.add_argument('-b', '--both', help="Search both the source and the destination (default)", action="store_true")
+an = parser.add_mutually_exclusive_group()
+an.add_argument('--noany', help="Ignore \'any\' in the ACLs", action="store_true")
+an.add_argument('--any', help="Show only \'any\' in the ACLs", action="store_true")
+dp = parser.add_mutually_exclusive_group()
+dp.add_argument('--deny', help="Search \'deny\' rules only" , action="store_true")
+dp.add_argument('--permit', help="Search \'permit\' rules only" , action="store_true")
+ra = parser.add_mutually_exclusive_group()
+ra.add_argument('--range', default=True, help="Replace lt, gt, and neq with ranges (default)", action="store_true")
+ra.add_argument('--norange', default=False, help="Replace lt, gt, and neq with \<, \>, and ! symbols", action="store_true")
+parser.add_argument('--direct', help="Direct IP match only" , action="store_true")
+parser.add_argument('-t','--transform', help='Transform the output. Must be used with either -s or -d and with either --deny or --permit', action="store_true")
+parser.add_argument('-p','--policy', help='Print the policy in the form:\n SourceIP SourceMask DestIP DestMask Proto:Port. Must be used with either --deny or --permit', action="store_true")
+parser.add_argument('--contain', help='Direct matches and subnets (not direct and uppernets). Assumes --noany', action="store_true")
+parser.add_argument('--noline', help='Removes line number from the output', action="store_true")
+args = parser.parse_args()
+
+if not args.src and not args.dst and not args.both: args.both = True
+if "all" in args.addr: args.addr="0.0.0.0/0"
+if "0.0.0.0/0" in args.addr and not args.any: args.contain=True
+if args.both and args.transform:
+	print >>sys.stderr, "--transform requires either --src or --dst. --transform cannot be used with --both"
+	sys.exit(1)
+if args.policy: args.transform=True
+if args.both and args.direct:
+	print >>sys.stderr, "--direct requires either --src or --dst. --both cannot be used with --direct"
+	sys.exit(1)
+
+if args.norange: args.range=False
+
+
+# service name - port mapping from
+# http://www.cisco.com/c/en/us/td/docs/security/asa/asa96/configuration/general/asa-96-general-config/ref-ports.html#ID-2120-000002b8
+if args.transform:
+	s2n={'domain': '53', 'sunrpc': '111', 'citrix-ica': '1494', 'telnet': '23', 'tftp': '69', 'syslog': '514', 'rtsp': '554', 'secureid-udp': '5510', 'gopher': '70', 'h323': '1720', 'echo': '7', 'netbios-ssn': '139', 'snmptrap': '162', 'rpc': '111', 'radius': '1645', 'pcanywhere-data': '5631', 'nameserver': '42', 'rsh': '514', 'sqlnet': '1521', 'uucp': '540', 'ftp': '21', 'sip': '5060', 'whois': '43', 'smtp': '25', 'ctiqbe': '2748', 'hostname': '101', 'snmp': '161', 'mobile-ip': '434', 'daytime': '13', 'ldaps': '636', 'isakmp': '500', 'netbios-dgm': '138', 'finger': '79', 'https': '443', 'ldap': '389', 'kshell': '544', 'irc': '194', 'nntp': '119', 'biff': '512', 'http': '80', 'cifs': '3020', 'exec': '512', 'pptp': '1723', 'ntp': '123', 'aol': '5190', 'talk': '517', 'pcanywhere-status': '5632', 'pop3': '110', 'pop2': '109', 'ftp-data': '20', 'lotusnotes': '1352', 'rip': '520', 'xdmcp': '177', 'pim-auto-rp': '496', 'login': '513', 'dnsix': '195', 'ident': '113', 'netbios-ns': '137', 'kerberos': '750', 'tacacs': '49', 'who': '513', 'cmd': '514', 'bootps': '67', 'bgp': '179', 'nfs': '2049', 'klogin': '543', 'chargen': '19', 'www': '80', 'time': '37', 'discard': '13', 'imap4': '143', 'lpd': '515', 'bootpc': '68', 'radius-acct': '1646', 'ssh': '22', 'redirect': '5', 'information-reply': '16', 'alternate-address': '6', 'mask-reply': '18', 'timestamp-request': '13', 'router-solicitation': '10', 'mobile-redirect': '32', 'parameter-problem': '12', 'echo': '8', 'timestamp-reply': '14', 'conversion-error': '31', 'information-request': '15', 'unreachable': '3', 'echo-reply': '0', 'source-quench': '4', 'mask-request': '17', 'time-exceeded': '11', 'router-advertisement': '9'}
 
 
 ips = []
